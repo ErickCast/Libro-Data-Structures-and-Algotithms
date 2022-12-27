1077d53cd55dca6efa71f54d67c5de5c73d20c36
ErickCast <erickcastillo193@gmail.com>
2022-12-26 20:22:36 -0700

    Se agrega metodo de libro

--- Sort Methods/Insertion Sort/index.js
+++ Sort Methods/Insertion Sort/index.js
@@ -1,14 +1,16 @@
 "use strict";
 function insertionSort(array) {
+    let steps = 0;
     for (let i = 1; i < array.length; i++) {
-        let tmpNumber = 0;
+        let tmpNumber = array[i];
         let consecutivo = i;
-        tmpNumber = array[i];
-        delete array[i];
         for (let y = i; y > 0; y--) {
+            steps++;
             if (array[y - 1] > tmpNumber) {
+                steps++;
                 array[y] = array[y - 1];
                 consecutivo--;
+                steps++;
             }
             else {
                 break;
@@ -17,8 +19,33 @@ function insertionSort(array) {
         array[consecutivo] = tmpNumber;
     }
     console.log(array);
+    console.log('Pasos realizados: ' + steps);
     return array;
 }
+// CODE IMPLEMENTATION BY BOOK "DATA STRUCTURES AND ALGORITHMS BY JAY WENGROW"
+/* function insertionSort(array:number[]):number[] {
+    let steps = 0;
+    for(let i = 1; i<array.length; i++) {
+        let tmpNumber = array[i];
+        let position = i - 1;
+        while(position >= 0) {
+            steps++;
+            if(array[position] > tmpNumber) {
+                steps++;
+                array[position + 1] = array[position];
+                position = position - 1;
+                steps++;
+            } else {
+                break;
+            }
+        }
+        array[position + 1] = tmpNumber;
+    }
+    console.log(array);
+    console.log('Pasos realizados: ' + steps)
+    return array;
+} */
+insertionSort([5, 4, 3, 2, 1]);
 insertionSort([4, 2, 8, 6, 9]);
 insertionSort([25, 267, 1, 7, 5]);
 insertionSort([4, 20505, 3, 5555, 2000, 34, 534354, 7667576765756, 4543354, 223, 43454, 365, 5663543546345543]);
